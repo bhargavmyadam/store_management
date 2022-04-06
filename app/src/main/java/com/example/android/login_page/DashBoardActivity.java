ee09d9b9ee4d9f3ecf6586b71fb813135596ac4f
M BHARGAV <bhargavmyadam@gmail.com>
2022-04-06 17:25:22 +0530

    11th commit added WorkersActivity.

--- app/src/main/java/com/example/android/login_page/DashBoardActivity.java
+++ app/src/main/java/com/example/android/login_page/DashBoardActivity.java
@@ -12,6 +12,7 @@ import com.example.android.login_page.Entity.Admin;
 public class DashBoardActivity extends AppCompatActivity {
     Admin admin;
     LinearLayout mAdminDetails;
+    LinearLayout mWorkers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +20,19 @@ public class DashBoardActivity extends AppCompatActivity {
         Intent intent = getIntent();
         admin = (Admin)intent.getSerializableExtra("admin");
         mAdminDetails = findViewById(R.id.ll_admin_details);
+        mWorkers = findViewById(R.id.ll_workers);
         mAdminDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(DashBoardActivity.this,AdminActivity.class);
+                intent.putExtra("admin",admin);
+                startActivity(intent);
+            }
+        });
+        mWorkers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DashBoardActivity.this,WorkersActivity.class);
                 intent.putExtra("admin",admin);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/android/login_page/WorkersActivity.java
+++ app/src/main/java/com/example/android/login_page/WorkersActivity.java
@@ -5,36 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.example.android.login_page.Entity.Admin;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import org.w3c.dom.Text;
+public class WorkersActivity extends AppCompatActivity {
 
-public class AdminActivity extends AppCompatActivity {
-    TextView mAdminName;
-    TextView mAdminEmail;
     Admin admin;
     FloatingActionButton mHomeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        setContentView(R.layout.activity_workers);
         Intent intent = getIntent();
         admin = (Admin)intent.getSerializableExtra("admin");
-        mAdminName = findViewById(R.id.tv_admin_name_value);
-        mAdminEmail = findViewById(R.id.tv_admin_email_value);
         mHomeButton = findViewById(R.id.bt_home);
         mHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(AdminActivity.this,DashBoardActivity.class);
-                intent1.putExtra("admin",admin);
-                startActivity(intent1);
+                Intent intent = new Intent(WorkersActivity.this,DashBoardActivity.class);
+                intent.putExtra("admin",admin);
+                startActivity(intent);
             }
         });
-        mAdminName.setText(admin.getAdminName());
-        mAdminEmail.setText(admin.getEmail());
     }
 }
