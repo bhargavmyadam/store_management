a12b4b276eb8c1a7e91bbcc629ed4974814090dd
M BHARGAV <bhargavmyadam@gmail.com>
2022-04-10 21:48:32 +0530

    16th commit added ui to worker view.

--- app/src/main/java/com/example/android/login_page/WorkerActivity.java
+++ app/src/main/java/com/example/android/login_page/WorkerActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.android.login_page.DataBaseHelper.DBHelper;
 import com.example.android.login_page.Entity.Admin;
 import com.example.android.login_page.Entity.Worker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,15 +20,14 @@ public class WorkerActivity extends AppCompatActivity {
     Worker worker;
     Admin admin;
     FloatingActionButton mHomeButton;
+    Button mRemove;
+    Button mEdit;
+    DBHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker);
-        mHomeButton = findViewById(R.id.bt_home);
         initFields();
-        Intent intent = getIntent();
-        worker = (Worker)intent.getSerializableExtra("worker");
-        admin = (Admin)intent.getSerializableExtra("admin");
         populateFields();
         mHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +37,31 @@ public class WorkerActivity extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
+        mEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mEdit.getText().toString().equals(getString(R.string.edit))){
+                    mEdit.setText(R.string.done);
+                    mRemove.setText(R.string.cancel);
+                    enableFields();
+                }
+                else{
+                    mEdit.setText(R.string.edit);
+                    mRemove.setText(R.string.remove);
+                    populateFields();
+                }
+            }
+        });
+    }
+
+    private void enableFields() {
+        mName.setEnabled(true);
+        mSalary.setEnabled(true);
+        mPhone1.setEnabled(true);
+        mPhone2.setEnabled(true);
+        mStreet.setEnabled(true);
+        mCity.setEnabled(true);
+        mHouseNumber.setEnabled(true);
     }
 
     private void populateFields() {
@@ -46,6 +72,14 @@ public class WorkerActivity extends AppCompatActivity {
         mStreet.setText(worker.getStreet());
         mCity.setText(worker.getCity());
         mHouseNumber.setText(worker.getHouseNumber());
+        mName.setEnabled(false);
+        mSalary.setEnabled(false);
+        mPhone1.setEnabled(false);
+        mPhone2.setEnabled(false);
+        mStreet.setEnabled(false);
+        mCity.setEnabled(false);
+        mHouseNumber.setEnabled(false);
+
     }
 
     private void initFields() {
@@ -56,6 +90,13 @@ public class WorkerActivity extends AppCompatActivity {
         mStreet = findViewById(R.id.et_street);
         mCity = findViewById(R.id.et_city);
         mHouseNumber = findViewById(R.id.et_house_number);
+        mRemove = findViewById(R.id.bt_remove);
+        mEdit = findViewById(R.id.bt_edit);
+        mHomeButton = findViewById(R.id.bt_home);
+        Intent intent = getIntent();
+        worker = (Worker)intent.getSerializableExtra("worker");
+        admin = (Admin)intent.getSerializableExtra("admin");
+        dbHelper = new DBHelper(this);
     }
 
 }
